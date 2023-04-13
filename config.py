1433980becdfc812585dd6eb0bcc28fd7534acb1
sitys-tgy <61150888+sitys-tgy@users.noreply.github.com>
2023-04-13 12:44:34 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "database-1.cqezgwk8aajx.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "sityiesian-employee"
+customregion = "us-east-1"
